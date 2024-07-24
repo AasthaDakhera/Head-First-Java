6267e5131e2f0b30a3aaf1921339c7bf4165953f
AasthaDakhera <aastha.dakhera@gmail.com>
2024-07-25 03:12:33 +0530

    Chapter 5 solutions (incomplete)

---
+++ Chapter 5/Mixed Messages(Pg.121)/Candidate_4.java
@@ -0,0 +1,20 @@
+//Output : 36 6
+
+public class Candidate_4 {
+    public static void main(String[] args) {
+        int x = 0;
+        int y = 30;
+        for (int outer = 0; outer < 3; outer++) {
+            for (int inner = 4; inner > 1; inner--) {
+                x++;            //Candidate code
+                y = y - 2;
+                if (x == 6) {
+                    break;
+                }
+                x = x + 3;
+            }
+            y = y - 2;
+        }
+        System.out.println(x + " " + y);
+    }
+}
